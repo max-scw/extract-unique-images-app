a11b960e635cb0dc82cc895b4eb4cb5532f88ccd
SCHWMAX <max.schwenzer@voith.com>
2024-10-29 15:33:54 +0100

    toy data + image descriptor

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # extract-unique-images-app
 Python's streamlit-based web-app to store only unique images
+
+
+Use the Python-Streamlit library to create a web-app that repeatedly triggers a camera (assume a request to a REST api), describes the image using a pre-trained neural network (use the small MobileNetV3 from torchvision) and compares it to stored images. 
+When a button it pressed, create a new folder and save all distinct images in it (i.e. the MSE is below a configurable threshold).
+
+
+## Structure
+
+The repository is structured as follows:
+``` 
+BaslerCameraAdapter
++-- toydata  <- contains pictures to demonstrate the app
+|-- LICENSE
+|-- README.md
+|-- requirements.txt  <- pip requirements
+```
---
+++ describe_images.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from PIL import Image
+
+import logging
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+
+from typing import Union, List, Dict
+
+
+torch.set_default_device("cpu")
+
+def build_descriptor_model():
+    # Load a pre-trained model
+    model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+    # Remove the classification layer (we only want the feature extractor)
+    model = nn.Sequential(*list(model.children())[:-1])
+    logging.info("Descriptor model built. Using a small MobileNetV3 pretrained on ImageNet.")
+    # Set the model to evaluation mode
+    model.eval()
+
+    # set up preprocessing
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    return model, preprocess
+
+
+def describe_image(p2img: Union[Path, str], model, preprocess) -> torch.tensor:
+    # load image as color image (preprocessing expects an image with 3 channels)
+    img = Image.open(p2img).convert("RGB")
+    # preprocess image to apply the NN
+    input_tensor = preprocess(img)
+    input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension
+    # Encode the image using the pre-trained model
+    with torch.no_grad():
+        encoded_features = model(input_batch)
+    return encoded_features.squeeze()
+
+
+def calculate_feature_diffs(features1, features_all) -> List[float]:
+    # calculate differences to already stored images
+    return [float(((features1 - val) ** 2).mean().sqrt()) for val in features_all]
